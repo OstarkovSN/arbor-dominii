2b0cee6e72b1bb5cf7e9fcfda8461672e04705fc
IgorZhiltsoff <zhiltsov.is@phystech.su>
2024-11-17 00:35:16 +0300

    need owner_id

--- main.py
+++ main.py
@@ -12,37 +12,41 @@ def kopeika(company_df, natural_df, founder_legal_df, founder_natural_df):
         founder_natural_df=founder_natural_df,
     )
 
+    print('Start2')
     indirect_shares = iteratively_estimate_indirect_shares(
         nonterminal=nonterminal,
         terminal=terminal,
     )
+    print('Done')
     
     # Сохраняем результаты
     os.makedirs('app/data/final', exist_ok=True)
 
-    with open("'app/data/final/output.tsv", "w") as f:
-        f.write("company_id\togrn\tinn\tfull_name\tbeneficiaries\n")
-        for company_id, shares in indirect_shares.items():
-            # TODO DELETE TERMINAL COMPNAIES
-            company = structure.companies[company_id]
-            beneficiaries = "\n".join(
-                f"{beneficiary_id}\t{share*100:.2f}%"
-                for beneficiary_id, share in shares.items()
-                if share > 0.25
-            )
-            f.write(
-                f"{company_id}\t{company['ogrn']}\t{company['inn']}\t{company['full_name']}\t{beneficiaries}\n"
-            )
+#    with open("'app/data/final/output.tsv", "w") as f:
+#        f.write("company_id\togrn\tinn\tfull_name\tbeneficiaries\n")
+#        for company_id, shares in indirect_shares.items():
+#            # TODO DELETE TERMINAL COMPNAIES
+#            company = structure.companies[company_id]
+#            beneficiaries = "\n".join(
+#                f"{beneficiary_id}\t{share*100:.2f}%"
+#                for beneficiary_id, share in shares.items()
+#                if share > 0.25
+#            )
+#            f.write(
+#                f"{company_id}\t{company['ogrn']}\t{company['inn']}\t{company['full_name']}\t{beneficiaries}\n"
+#            )
+#
 
 def mk_natural(founder_natural_df):
-    narutal_df = pd.DataFrame()
-    narutal_df['full_name'] =  \
+    natural_df = pd.DataFrame()
+    natural_df['full_name'] =  \
      founder_natural_df['last_name'] + ' ' \
       + founder_natural_df['first_name'] + ' ' \
        + founder_natural_df['second_name']
     
-    natural_df['inn'] = founder_natural_df['inn'].drop_duplicates()
-    natural_df['full_credentials'] = natural_df['inn'] + '#' + narutal_df['full_name']
+    natural_df['inn'] = founder_natural_df['inn']
+    natural_df = natural_df.drop_duplicates()
+    natural_df['full_credentials'] = natural_df['inn'].astype(str) + '#' + natural_df['full_name']
     return natural_df
 
 
@@ -60,4 +64,5 @@ if __name__ == "__main__":
         print(f"Отсутствующие company_id в company.tsv: {missing_company_ids}")
 
 
-    kopeika(company_df=company_df, founder_legal_df=founder_legal_df, founder_natural_df=founder_natural_df)
+    print('Start1')
+    kopeika(company_df=company_df, natural_df=natural_df, founder_legal_df=founder_legal_df, founder_natural_df=founder_natural_df)
