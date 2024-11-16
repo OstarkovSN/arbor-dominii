15185dd230c8d154254241be3f42a5eb78f81019
Hamyrappy <k.v.t10@mail.ru>
2024-11-16 19:59:27 +0300

    Изменения в препроцессинге

--- app/common/preprocess.py
+++ app/common/preprocess.py
@@ -1,5 +1,6 @@
 import os
 from typing import Sequence
+import pandas as pd
 
 class Preprocessor:
     '''
@@ -52,9 +53,14 @@ class QMCloser(Preprocessor):
         processed_lines = []
 
         for line in lines:
-            open_quotes = line.count('"') % 2 != 0  # Проверяем, есть ли незакрытая кавычка
-            if open_quotes:
-                line += '"'  # Закрываем кавычку в конце строки
+            values = line.split('\t')
+            if line.count('"') % 2 != 0:  # Проверяем, есть ли незакрытая кавычка в третьем значении
+                for i, value in enumerate(values):
+                    try:
+                        float(value)
+                    except ValueError:
+                        values[i] += '"'  # Закрываем кавычку
+                        break
             processed_lines.append(line)
 
         return '\n'.join(processed_lines)
@@ -97,5 +103,71 @@ def preprocess_default():
     preprocessors = [CompanyNamesMerger(), QMCloser()]
     preprocess_folder(folder_path, output_folder_path, preprocessors)
 
+def get_dfs(folder_path='data/processed'):
+
+    df_company = pd.read_table(
+        'app\\data\\processed\\company.tsv',
+        header=0,
+        dtype={
+            'id': 'Int64',
+            'ogrn': 'string',
+            'inn': 'string',
+            'full_name': 'string'
+        },
+        na_values=['', ' ', 'NA', 'nan']
+    )
+
+    df_founder_natural = pd.read_table(
+        'app\\data\\processed\\founder_natural.tsv',
+        header=0,
+        dtype={
+            'id': 'Int64',
+            'company_id': 'Int64', # id компании которой владеют
+            'inn': 'string',
+            'last_name': 'string',
+            'first_name': 'string',
+            'second_name': 'string',
+            'share': 'float',
+            'share_percent': 'float'
+        },
+        na_values=['', ' ', 'NA', 'nan']
+    )
+    
+
+    df_founder_legal = pd.read_table(
+        'app\\data\\processed\\founder_legal.tsv',
+        header=0,
+        dtype={
+            'id': 'Int64',
+            'company_id': 'Int64', # id компании которой владеют
+            'ogrn': 'string',
+            'inn': 'string',
+            'full_name': 'string',
+            'share': 'float',
+            'share_percent': 'float'
+        },
+        na_values=['', ' ', 'NA', 'nan']
+    )
+
+    if df_company['ogrn'].duplicated().any():
+        raise ValueError("Столбец 'ogrn' в df_company содержит дубликаты.")
+
+    # * Добавляем колонку owner_id
+    df_founder_legal = df_founder_legal.merge( 
+        df_company[['ogrn', 'id']].rename(columns={'id': 'owner_id'}),
+        on='ogrn',
+        how='left'
+    )
+
+    return df_company, df_founder_legal, df_founder_natural
+
+
+
+
 if __name__ == '__main__':
     preprocess_default()
+
+    df_company, df_founder_legal, df_founder_natural = get_dfs()
+
+    for df in (df_company, df_founder_legal, df_founder_natural):
+        print(df.head().to_string())
